a6f5dd1cc3e1c38c9c0fa564b0739eeea00099c9
ruanha <rune_hartvig@hotmail.com>
2018-11-27 17:20:41 +0100

    Init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+const express = require('express')
+
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath)
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.static(publicPath))
+
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`)
+})
